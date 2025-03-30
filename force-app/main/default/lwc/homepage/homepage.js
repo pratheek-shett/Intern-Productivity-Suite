af39ce0f33ec33994762a1292256f840cb76b96d
Pratheek Shetty <pratheekshetty9944@gmail.com>
2025-03-30 21:07:59 +0530

    Repo transfered

---
+++ force-app/main/default/lwc/homepage/homepage.js
@@ -0,0 +1,71 @@
+import { LightningElement } from 'lwc';
+import Sessioncreation from '@salesforce/apex/Sessioncontroler.Sessioncreation';
+import Sessionrefresh from '@salesforce/apex/Sessioncontroler.Sessionrefresh';
+import Sessionrevalidate from '@salesforce/apex/Sessioncontroler.Sessionrevalidate';
+import returnemailforauth from '@salesforce/apex/Sessioncontroler.returnemailforauth';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class Homepage extends NavigationMixin(LightningElement) {
+
+    sessioninterval;
+    email;
+    refreshHandler;
+
+    returnemail(){
+         returnemailforauth().then(data => {
+            this.email = data;
+        });
+
+    }
+
+    connectedCallback(){
+            this.refreshHandler = () => Sessionrefresh({getemail : this.email});
+            window.addEventListener('click', this.refreshHandler);
+            window.addEventListener('scroll', this.refreshHandler);
+            window.addEventListener('keypress', this.refreshHandler);
+            this.returnemail();
+
+            setTimeout(() => {
+                this.checkSession();
+                this.sessioninterval = setInterval(() => this.checkSession(), 30000);
+            }, 500);
+            // this.checkSession();
+            // this.sessioninterval = setInterval(() => this.checkSession(), 30000);
+    }
+
+
+    disconnectedcallback(){
+        window.removeEventListener('click', this.refreshHandler);
+         window.removeEventListener('scroll', this.refreshHandler);
+         window.removeEventListener('keypress', this.refreshHandler);
+        clearInterval(this.sessioninterval);
+    }
+
+    
+
+
+    async checkSession() {
+        try {
+            const isValid = await Sessionrevalidate({getemail : this.email});
+            if (!isValid) {
+                this.navigatetologin();
+            }
+        } catch(error) {
+            console.error('Session check failed:', error);
+            this.navigatetologin();
+        }
+    }
+
+    navigatetologin(e){
+    this[NavigationMixin.Navigate]({
+        type: 'standard__webPage',
+    attributes: {
+        url: '/' 
+    }
+    })
+}
+
+
+
+
+}
